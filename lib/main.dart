88cc3ac894e4ba6f78b1a0d25421b45fd2664321
Gabriel <contactodark84@gmail.com>
2025-02-20 16:13:37 +0100

    main + registro

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:streamhub/main_view.dart';
+import 'Registro/registro.dart';
+import 'Registro/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Menu(),
+      theme: ThemeData(
+        fontFamily: 'Roboto',
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(6, 13, 23, 1), // Color sólido de fondo
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "¡Bienvenido!",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 30),
+              buildButton(context, "Sin Registro", Colors.orange, () {}),
+              buildButton(context, "Registrarse", Colors.orange, () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegistroScreen()),
+                );
+              }),
+              buildButton(context, "Login", Colors.orange, () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen(correo: '', password: '')),
+                );
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton(BuildContext context, String text, Color color, VoidCallback onPressed) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: SizedBox(
+        width: 260,
+        height: 60,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: color,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            elevation: 5,
+            shadowColor: Colors.black,
+            textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          child: Text(text, style: TextStyle(color: Colors.black)), // Texto en negro
+        ),
+      ),
     );
   }
 }

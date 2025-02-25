9844a32eb93c37735f5c8fabc14fd37a87f0bc96
Gabriel <contactodark84@gmail.com>
2025-02-25 17:23:48 +0100

    Update registro.dart

---
+++ lib/Registro/registro.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '/main.dart';
+import '/Registro/login.dart';
+
+class RegistroScreen extends StatelessWidget {
+  final TextEditingController nombreController = TextEditingController();
+  final TextEditingController apellidoController = TextEditingController();
+  final TextEditingController usuarioController = TextEditingController();
+  final TextEditingController correoController = TextEditingController();
+  final TextEditingController telefonoController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+}

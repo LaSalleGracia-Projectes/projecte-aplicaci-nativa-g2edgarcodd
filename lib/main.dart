9f5a1498255101e690a138069ccee9b1af9305de
Gabriel <contactodark84@gmail.com>
2025-02-25 16:12:41 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,33 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(6, 13, 23, 1), // Color sólido de fondo
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "¡Bienvenido!",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 30),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+

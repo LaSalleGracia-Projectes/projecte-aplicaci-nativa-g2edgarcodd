a41b7349a9d9dbd20df1ddecca356640fffa9f36
Adrián Ortega <adrianortegaolgoso@gmail.com>
2025-03-04 15:20:07 +0100

    misc: commit para subir lo ultimo

--- lib/main_view.dart
+++ lib/main_view.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Menu extends StatelessWidget {
+class Menu extends StatefulWidget {
   const Menu({super.key});
 
   @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+  final int _totalPages = 2;
+  late Timer _timer;
+
+  void initState() {
+    super.initState();
+    _startAutoSlide();
+  }
+
+  void _startAutoSlide() {
+    _timer = Timer.periodic(Duration(seconds: 5), (Timer timer) {
+      setState(() {
+        _currentPage = (_currentPage + 1) % _totalPages;
+        _pageController.animateToPage(
+          _currentPage,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+        );
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _navigateToPage(int page) {
+    setState(() {
+      _currentPage = page;
+      _pageController.animateToPage(
+        page,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Stack(
@@ -14,6 +62,7 @@ class Menu extends StatelessWidget {
               alignment: Alignment.centerLeft,
               child: Image.asset('images/logoPrueba.png', height: 40),
             ),
+
             Align(
               alignment: Alignment.center,
               child: Row(
@@ -21,19 +70,31 @@ class Menu extends StatelessWidget {
                 children: [
                   TextButton(
                     onPressed: () {},
-                    child: Text('Inicio', style: TextStyle(color: Color(0xFFF6F6F7), fontSize: 16)),
+                    child: Text(
+                      'Inicio',
+                      style: TextStyle(color: Color(0xFFF6F6F7), fontSize: 16),
+                    ),
                   ),
+
                   TextButton(
                     onPressed: () {},
-                    child: Text('Servicios', style: TextStyle(color: Color(0xFFF6F6F7), fontSize: 16)),
+                    child: Text(
+                      'Servicios',
+                      style: TextStyle(color: Color(0xFFF6F6F7), fontSize: 16),
+                    ),
                   ),
+
                   TextButton(
                     onPressed: () {},
-                    child: Text('Contacto', style: TextStyle(color: Color(0xFFF6F6F7), fontSize: 16)),
+                    child: Text(
+                      'Contacto',
+                      style: TextStyle(color: Color(0xFFF6F6F7), fontSize: 16),
+                    ),
                   ),
                 ],
               ),
             ),
+
             Align(
               alignment: Alignment.centerRight,
               child: Row(
@@ -49,11 +110,14 @@ class Menu extends StatelessWidget {
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20),
                         ),
+
                         contentPadding: EdgeInsets.symmetric(horizontal: 10),
                       ),
                     ),
                   ),
+
                   SizedBox(width: 10),
+
                   GestureDetector(
                     onTap: () => Scaffold.of(context).openEndDrawer(),
                     child: CircleAvatar(
@@ -65,26 +129,34 @@ class Menu extends StatelessWidget {
             ),
           ],
         ),
+
         backgroundColor: Color(0xFF060D17),
       ),
+
       endDrawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
             DrawerHeader(
               decoration: BoxDecoration(color: Color(0xFF060D17)),
-              child: Text('Menú de usuario', style: TextStyle(color: Color(0xFFF6F6F7))),
+              child: Text(
+                'Menú de usuario',
+                style: TextStyle(color: Color(0xFFF6F6F7)),
+              ),
             ),
+
             ListTile(
               leading: Icon(Icons.person),
               title: Text('Perfil'),
               onTap: () {},
             ),
+
             ListTile(
               leading: Icon(Icons.settings),
               title: Text('Configuración'),
               onTap: () {},
             ),
+
             ListTile(
               leading: Icon(Icons.exit_to_app),
               title: Text('Cerrar sesión'),
@@ -93,7 +165,197 @@ class Menu extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(child: Text('Contenido principal')),
+
+      body: Container(
+        color: Color(0xFF060D17),
+        child: Center(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.6,
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: PageView(
+              controller: _pageController,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: Center(
+                        child: Image.asset(
+                          'images/logoPrueba.png',
+                          width: 150,
+                          height: 150,
+                        ),
+                      ),
+                    ),
+
+                    Expanded(
+                      flex: 6,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              CircleAvatar(
+                                radius: 50,
+                                backgroundImage: AssetImage(
+                                  'images/logoPrueba.png',
+                                ),
+                              ),
+                              SizedBox(width: 20),
+                              Expanded(
+                                child: Stack(
+                                  children: [
+                                    SizedBox(
+                                      height: 100,
+                                      child: Text(
+                                        'Lorem ipsum odor amet, consectetuer adipiscing elit. Dictum cras et tellus nulla semper quam velit lacus. Cursus non eget maximus dignissim sagittis facilisis. Platea donec senectus ut augue ornare et aenean quis. Accumsan ultrices non quisque rutrum netus nulla vel. Sem ornare venenatis interdum aliquet natoque semper elit felis. Eros pretium volutpat congue vitae vitae massa. Dictumst cubilia ad laoreet cursus quam placerat convallis class. Mauris gravida eget efficitur massa diam semper. Lorem ipsum odor amet, consectetuer adipiscing elit. Dictum cras et tellus nulla semper quam velit lacus. Cursus non eget maximus dignissim sagittis facilisis. Platea donec senectus ut augue ornare et aenean quis. Accumsan ultrices non quisque rutrum netus nulla vel. Sem ornare venenatis interdum aliquet natoque semper elit felis. Eros pretium volutpat congue vitae vitae massa. Dictumst cubilia ad laoreet cursus quam placerat convallis class. Mauris gravida eget efficitur massa diam semper.',
+                                        style: TextStyle(fontSize: 16, color: Color(0xFFF6F6F7)),
+                                        maxLines: 8,
+                                        overflow: TextOverflow.fade,
+                                        softWrap: true,
+                                      ),
+                                    ),
+                                    Positioned(
+                                      bottom: 0,
+                                      left: 0,
+                                      right: 0,
+                                      child: Container(
+                                        height: 20,
+                                        decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                            begin: Alignment.topCenter,
+                                            end: Alignment.bottomCenter,
+                                            colors: [
+                                              Colors.transparent,
+                                              Color(0xFF060D17).withValues(alpha: 0.5),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: () {
+                              /*Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => OtraVista(),
+                                ),
+                              );*/
+                            },
+                            child: Text('Leer más'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: Center(
+                        child: Image.asset(
+                          'images/logoPrueba.png',
+                          width: 150,
+                          height: 150,
+                        ),
+                      ),
+                    ),
+
+                    Expanded(
+                      flex: 6,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              CircleAvatar(
+                                radius: 50,
+                                backgroundImage: AssetImage(
+                                  'images/logoPrueba.png',
+                                ),
+                              ),
+                              SizedBox(width: 20),
+                              Expanded(
+                                child: Stack(
+                                  children: [
+                                    SizedBox(
+                                      height: 100,
+                                      child: Text(
+                                        'Lorem ipsum odor amet, consectetuer adipiscing elit. Dictum cras et tellus nulla semper quam velit lacus. Cursus non eget maximus dignissim sagittis facilisis. Platea donec senectus ut augue ornare et aenean quis. Accumsan ultrices non quisque rutrum netus nulla vel. Sem ornare venenatis interdum aliquet natoque semper elit felis. Eros pretium volutpat congue vitae vitae massa. Dictumst cubilia ad laoreet cursus quam placerat convallis class. Mauris gravida eget efficitur massa diam semper. Lorem ipsum odor amet, consectetuer adipiscing elit. Dictum cras et tellus nulla semper quam velit lacus. Cursus non eget maximus dignissim sagittis facilisis. Platea donec senectus ut augue ornare et aenean quis. Accumsan ultrices non quisque rutrum netus nulla vel. Sem ornare venenatis interdum aliquet natoque semper elit felis. Eros pretium volutpat congue vitae vitae massa. Dictumst cubilia ad laoreet cursus quam placerat convallis class. Mauris gravida eget efficitur massa diam semper.',
+                                        style: TextStyle(fontSize: 16, color: Color(0xFFF6F6F7)),
+                                        maxLines: 8,
+                                        overflow: TextOverflow.fade,
+                                        softWrap: true,
+                                      ),
+                                    ),
+                                    Positioned(
+                                      bottom: 0,
+                                      left: 0,
+                                      right: 0,
+                                      child: Container(
+                                        height: 20,
+                                        decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                            begin: Alignment.topCenter,
+                                            end: Alignment.bottomCenter,
+                                            colors: [
+                                              Colors.transparent,
+                                              Color(0xFF060D17).withValues(alpha: 0.5),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: () {
+                              /*Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => OtraVista(),
+                                ),
+                              );*/
+                            },
+                            child: Text('Leer más'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                // Puedes agregar más páginas aquí
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 }

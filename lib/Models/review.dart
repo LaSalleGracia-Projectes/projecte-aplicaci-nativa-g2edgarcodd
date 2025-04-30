3c43c2a1f1fd7f998f1d8c7fbf8c864e2e8e3155
Gabriel <contactodark84@gmail.com>
2025-04-30 16:32:45 +0200

    Update review.dart

--- lib/Models/review.dart
+++ lib/Models/review.dart
@@ -1,1 +1,17 @@
- 
+class Review {
+  final String author;
+  final String content;
+  final String? avatarPath;
+  final double rating;
+  final String createdAt;
+  final bool isPositive;
+
+  Review({
+    required this.author,
+    required this.content,
+    this.avatarPath,
+    required this.rating,
+    required this.createdAt,
+    required this.isPositive,
+  });
+} 
